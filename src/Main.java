13f68452d30516623d58b3c4e5136633d99789c7
Yusuf Sahin <yusuf.s@gmx.de>
2024-10-08 18:17:36 +0200

    Add some animation in Main class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 
 import javax.swing.*;
+import java.util.Arrays;
 import java.util.concurrent.ExecutorService;
 import java.awt.*;
 import java.util.concurrent.Executor;
@@ -12,9 +13,9 @@ public class Main extends JApplet {
     private String animationName = "";
 
     public Main() {
-        setLayout(new GridLayout(1,1,0,0));
+        setLayout(new GridLayout(1, 1, 0, 0));
         SortPanelsHolder sortPanelsHolder = new SortPanelsHolder();
-        sortPanelsHolder.setLayout(new GridLayout(0,3,0,0));
+        sortPanelsHolder.setLayout(new GridLayout(0, 3, 0, 0));
         sortPanelsHolder.setBackground(Color.BLACK);
         sortPanelsHolder.setForeground(Color.BLACK);
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
@@ -28,7 +29,138 @@ public class Main extends JApplet {
         sortPanels[5] = new QuickSortPanel("Quick Sort", sleepTime, width, height);
         sortPanels[6] = new RadixSortPanel("Radix Sort", sleepTime, width, height);
 
+        for (int i = 0; i < sortPanels.length; i++) {
+            sortPanels[i].setVisible(false);
+            sortPanelsHolder.add(sortPanels[i]);
+
+        }
+
+        add(sortPanelsHolder);
+
 
     }
 
+    class SortPanelsHolder extends JPanel{
+        private static final long serialVersionUID = 1L;
+        @Override
+        protected void paintComponent(Graphics g){
+            super.paintComponent(g);
+            g.setColor(Color.GREEN);
+            Font animationNameFont = new Font(Font.MONOSPACED, Font.BOLD, 150);
+            FontMetrics animationNameFontMetrics = getFontMetrics(animationNameFont);
+            g.setFont(animationNameFont);
+            int x = (getWidth() - animationNameFontMetrics.stringWidth(animationName)) / 2;
+            int y  = (getHeight() - animationNameFontMetrics.getLeading()) / 2;
+            g.drawString(animationName, x, y);
+        }
+    }
+
+
+    public void beginAnimation(String animationName, int[] list){
+        try{
+            this.animationName = animationName;
+            repaint();
+            Thread.sleep(200);
+            this.animationName = "";
+            repaint();
+            for (int i = 0; i <sortPanels.length; i++){
+                sortPanels[i].setList(list);
+                sortPanels[i].setVisible(true);
+            }
+            Thread.sleep(1000);
+            ExecutorService executor = Executor.newFixedThreadPool(sortPanels.length);
+            for (int i= 0; i< sortPanels.length; i++){
+                executor.execute(sortPanels[i]);
+            }
+            executor.shutdown();
+            while (!executor.isTerminated()){
+                Thread.sleep(100);
+            }
+            Thread.sleep(1000);
+            for (int i = 0 ; i< sortPanels.length; i++){
+                sortPanels[i].setVisible(false);
+            }
+        } catch (InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args){
+        JFrame frame = new JFrame("Sorting algorithms");
+        Main main = new Main();
+        frame.add(main);
+        frame.setUndecorated(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+
+        int[] list = new int[size];
+
+        for (int i = 0; i< list.length; i++){
+            list[i] = i+1;
+        }
+
+        for (int i = 0; i< list.length; i++){
+            int index = (int)(Math.random() * list.length);
+            int temp = list[i];
+            list[i] = list[index];
+            list[index] = temp;
+        }
+
+        main.beginAnimation("Random", list);
+
+        for (int i = 0; i< list.length; i++){
+            list[i] = (1+ i / (size /4) * (size / 4));
+        }
+
+        for (int i = 0; i < list.length; i++){
+            int index = (int)(Math.random() * list.length);
+            int temp = list[i];
+            list[i] = list[index];
+            list[index] = temp;
+        }
+
+        main.beginAnimation("few unique", list);
+
+        for (int i = 0; i<list.length; i++){
+            list[i] = size - i;
+        }
+
+        main.beginAnimation("Reversed", list);
+
+        for (int i = 0; i < list.length / 2; i++){
+            list[i] = i + 1;
+        }
+        for (int i  = list.length / 2; i<list.length; i++){
+            list[i] = i + 2;
+        }
+        list[list.length - 1]= list.length / 2 + 1;
+        main.beginAnimation("Almost Sorted", list);
+
+    }
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/SelectionSortPanel.java
@@ -0,0 +1,2 @@
+public class SelectionSortPanel {
+}
