e709aa7908a3754fa88f701af5e49fd140306d71
Yusuf Sahin <yusuf.s@gmx.de>
2024-10-11 16:43:29 +0200

    Change the code to be much more efficient

--- src/BubbleSortPanel.java
+++ src/BubbleSortPanel.java
@@ -1,76 +1,50 @@
-import java.awt.Color;
-import java.awt.Graphics;
-import java.lang.annotation.Inherited;
+import java.awt.*;
 
 public class BubbleSortPanel extends SortPanel {
-    private static final long serialVersionUID = 1L;
-    private int redColumn = -1;
-    private int greenColumn = -1;
+    private int currentIndex = -1;
 
-    public BubbleSortPanel(String name, int sleepTime, int width, int height){
-        super(name, sleepTime, width, height);
+    public BubbleSortPanel(int size, int width, int height){
+        super(size,width,height);
     }
 
+    @Override
     public void reset(){
-        redColumn = -1;
-        greenColumn = -1;
+        currentIndex = -1;
     }
 
     @Override
-    public void run(){
-        try {
-            boolean needNextPass = true;
-            for (int k = 1; k < list.length && needNextPass; k++){
-                needNextPass = false;
-                for (int i = 0; i < list.length; i++){
-                    redColumn = i;
-                    repaint();
-                    Thread.sleep(4 * sleepTime);
-                    if ( list[i] > list[i -1]){
-                        redColumn = i + 1;
-                        int temp = list[i];
-                        list[i] = list[i+1];
-                        list[i+1] = temp;
-                        repaint();
-                        Thread.sleep(4 * sleepTime);
-                        needNextPass = true;
-                    }
+    public void sort() throws InterruptedException {
+        boolean swapped;
+        for (int i = 0; i < list.length - 1; i++) {
+            swapped = false;
+            for (int j = 0; j < list.length - 1 - i; j++) {
+                currentIndex = j;
+                if (list[j] > list[j + 1]) {
+                    int temp = list[j];
+                    list[j] = list[j + 1];
+                    list[j + 1] = temp;
+                    swapped = true;
                 }
-                greenColumn = size - k;
+                repaint();
+                Thread.sleep(sleepTime);
             }
-            greenColumn = 0;
-            redColumn = -1;
-        } catch (InterruptedException e){
-
+            if (!swapped) {
+                break;
+            }
         }
-
-        repaint();
     }
 
+
+
     @Override
-    protected void paintComponent(Graphics g){
+    public void paintComponent(Graphics g){
         super.paintComponent(g);
-        int columnWidth = (getWidth() - 4 * BORDER_WIDTH) / size;
-        int columnHeight = (getHeight() - 4 * BORDER_WIDTH) / size;
-        for (int i = 0; i < (greenColumn == -1 ? list.length : greenColumn); i++){
-            g.setColor(Color.WHITE);
-            g.fillRect(2 * BORDER_WIDTH + columnWidth*i, getHeight() - list[i] * columnHeight - 2 * BORDER_WIDTH, columnWidth, list[i] * columnHeight);
-            g.setColor(Color.BLACK);
-            g.drawRect(2 * BORDER_WIDTH + columnWidth*i, getHeight() - list[i] * columnHeight - 2 * BORDER_WIDTH, columnWidth, list[i] * columnHeight);
-        }
-        if (greenColumn != -1){
-            for (int i = greenColumn; i < list.length; i++){
-                g.setColor(Color.GREEN);
-                g.fillRect(2 * BORDER_WIDTH + columnWidth*i, getHeight() - list[i] * columnHeight - 2 * BORDER_WIDTH, columnWidth, list[i] * columnHeight);
-                g.setColor(Color.BLACK);
-                g.drawRect(2 * BORDER_WIDTH + columnWidth*i, getHeight() - list[i] * columnHeight - 2 * BORDER_WIDTH, columnWidth, list[i] * columnHeight);
-            }
-        }
-        if ( redColumn != -1){
+        if (currentIndex != -1){
             g.setColor(Color.RED);
-            g.fillRect( 2 * BORDER_WIDTH + columnWidth * redColumn, getHeight() - list[redColumn] * columnHeight - 2 * BORDER_WIDTH, columnWidth, list[redColumn] * columnHeight);
-            g.setColor(Color.BLACK);
-            g.drawRect(2 * BORDER_WIDTH + columnWidth * redColumn, getHeight() - list[redColumn] * columnHeight - 2 * BORDER_WIDTH, columnWidth, list[redColumn] * columnHeight);
+            int barWidth = getWidth() / size;
+            int barHeight = (int) (((double) list[currentIndex] /size) * getHeight());
+            g.fillRect(currentIndex * barWidth, getHeight() - barHeight, barWidth, barHeight);
         }
     }
 }
+
